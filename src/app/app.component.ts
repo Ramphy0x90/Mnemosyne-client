fbaeb742bb14cb87af64349b99db9b6a5c0fb5bf
ramphy0x90 <ramphy11@hotmail.com>
2023-06-04 16:12:28 +0200

    Route animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouteTransitionAnimations } from './route-transition-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [RouteTransitionAnimations],
 })
 export class AppComponent {
   title = 'Mnemosyne_client';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData['animationState']
+    );
+  }
 }
