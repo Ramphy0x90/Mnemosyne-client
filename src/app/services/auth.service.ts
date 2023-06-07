4bc9930e4a727a1ce67acf6b51846d70eeb7b3fc
ramphy0x90 <ramphy11@hotmail.com>
2023-06-07 23:17:14 +0200

    Auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthLogin } from '../models/auth/auth-login';
 import { AuthSignUp } from '../models/auth/auth-sign-up';
+import { AuthResponse } from '../models/auth/auth-response';
+import { AuthState } from '../constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  /**
+   * Broadcast auth user status to subscribers
+   */
+  userAuthStatus: EventEmitter<AuthState> = new EventEmitter();
+
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
@@ -39,5 +46,31 @@ export class AuthService {
       user,
       this.httpOptions
     );
+  }
+
+  /**
+   * This function is used to store the user token
+   * and broadcast the authentication state
+   * @param AuthResponse
+   */
+  storeUserToken(AuthResponse: AuthResponse): void {
+    window.sessionStorage.clear();
+
+    this.userAuthStatus.emit(AuthState.AUTHENTICATED);
+
+    window.sessionStorage.setItem('token', AuthResponse.token);
+    // window.sessionStorage.setItem('user', JSON.stringify(user));
+  }
+
+  /**
+   * Get user authentication status
+   * @returns AuthState
+   */
+  getUserAuthStatus(): AuthState {
+    if (window.sessionStorage.getItem('token') !== null) {
+      return AuthState.AUTHENTICATED;
+    }
+
+    return AuthState.UNAUTHENTICATED;
   }
 }
