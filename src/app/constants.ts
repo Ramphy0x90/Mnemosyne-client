e1120f4ac7341c8c5399485f01e03ea43d8d6901
ramphy0x90 <ramphy11@hotmail.com>
2023-06-07 23:16:38 +0200

    Consts

---
+++ src/app/constants.ts
@@ -0,0 +1,13 @@
+export const enum AuthState {
+  AUTHENTICATED,
+  UNAUTHENTICATED,
+  LOGGED_OUT,
+}
+
+export const enum ShowNavOptionOn {
+  USER_AUTHENTICATED,
+  USER_UNAUTHENTICATED,
+  USER_ADMIN,
+  ALL,
+  TEST,
+}
