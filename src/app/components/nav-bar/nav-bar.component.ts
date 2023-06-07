d781df7b7ba07c2ad27f67b1220332739abf4d39
ramphy0x90 <ramphy11@hotmail.com>
2023-06-07 23:16:30 +0200

    Nav options check

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthState, ShowNavOptionOn } from 'src/app/constants';
 import { NavComponent } from 'src/app/models/navigation/nav-component';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,18 +9,48 @@ import { NavComponent } from 'src/app/models/navigation/nav-component';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  userLogged: boolean = false;
+
   navOptions: NavComponent[] = [
     {
       name: 'Login',
       route: 'log-in',
+      visible: ShowNavOptionOn.USER_UNAUTHENTICATED,
     },
     {
       name: 'Signup',
       route: 'sign-up',
+      visible: ShowNavOptionOn.USER_UNAUTHENTICATED,
     },
   ];
 
-  constructor() {}
+  constructor(private authService: AuthService) {
+    authService.userAuthStatus.subscribe({
+      next: (userAuthStatus: AuthState) => {
+        this.userLogged = userAuthStatus == AuthState.AUTHENTICATED;
+      },
+    });
+  }
 
   ngOnInit(): void {}
+
+  optionVisible(showOn: ShowNavOptionOn): boolean {
+    switch (showOn) {
+      case ShowNavOptionOn.USER_AUTHENTICATED:
+        return this.userLogged;
+
+      case ShowNavOptionOn.USER_UNAUTHENTICATED:
+        return this.userLogged;
+
+      case ShowNavOptionOn.USER_ADMIN:
+        return false;
+
+      default:
+        return false;
+    }
+  }
+
+  userAuthenticated(): boolean {
+    return this.userLogged;
+  }
 }
