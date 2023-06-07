22770ea53b909ac1a94cd80a502e9d5c90f4545f
ramphy0x90 <ramphy11@hotmail.com>
2023-06-07 23:16:11 +0200

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './containers/log-in/log-in.component';
 import { SignUpComponent } from './containers/sign-up/sign-up.component';
+import { TestComponent } from './containers/dev/test/test.component';
+import { AuthAdminGuard } from './guards/auth-admin.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'log-in', component: LogInComponent },
   { path: 'sign-up', component: SignUpComponent },
+  {
+    path: 'dev',
+    canActivate: [AuthAdminGuard],
+    children: [{ path: 'test', component: TestComponent }],
+  },
 ];
 
 @NgModule({
