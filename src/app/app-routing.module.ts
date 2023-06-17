63db51581b2aa73ec952720516b5e43062ee81d9
ramphy0x90 <ramphy11@hotmail.com>
2023-06-17 17:38:30 +0200

    Reformat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LogInComponent } from './containers/log-in/log-in.component';
-import { SignUpComponent } from './containers/sign-up/sign-up.component';
-import { TestComponent } from './containers/dev/test/test.component';
-import { AuthAdminGuard } from './guards/auth-admin.guard';
-import { CloudComponent } from './containers/cloud/cloud.component';
-import { AuthGuard } from './guards/auth.guard';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LogInComponent } from "./containers/log-in/log-in.component";
+import { SignUpComponent } from "./containers/sign-up/sign-up.component";
+import { TestComponent } from "./containers/dev/test/test.component";
+import { AuthAdminGuard } from "./guards/auth-admin.guard";
+import { CloudComponent } from "./containers/cloud/cloud.component";
+import { AuthGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'log-in', component: LogInComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  { path: 'cloud', component: CloudComponent, canActivate: [AuthGuard] },
-  {
-    path: 'dev',
-    canActivate: [AuthAdminGuard],
-    children: [{ path: 'test', component: TestComponent }],
-  },
+	{ path: "", pathMatch: "full", redirectTo: "home" },
+	{ path: "log-in", component: LogInComponent },
+	{ path: "sign-up", component: SignUpComponent },
+	{ path: "cloud", component: CloudComponent, canActivate: [AuthGuard] },
+	{
+		path: "dev",
+		canActivate: [AuthAdminGuard],
+		children: [{ path: "test", component: TestComponent }],
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes, { useHash: true })],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouteTransitionAnimations } from './route-transition-animations';
-import { AuthService } from './services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { RouteTransitionAnimations } from "./route-transition-animations";
+import { AuthService } from "./services/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [RouteTransitionAnimations],
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
+	animations: [RouteTransitionAnimations],
 })
 export class AppComponent implements OnInit {
-  title = 'Mnemosyne_client';
+	title = "Mnemosyne_client";
 
-  constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
-    this.authService.recoverAuthStatus();
-  }
+	ngOnInit(): void {
+		this.authService.recoverAuthStatus();
+	}
 
-  prepareRoute(outlet: RouterOutlet) {
-    return (
-      outlet &&
-      outlet.activatedRouteData &&
-      outlet.activatedRouteData['animationState']
-    );
-  }
+	prepareRoute(outlet: RouterOutlet) {
+		return (
+			outlet &&
+			outlet.activatedRouteData &&
+			outlet.activatedRouteData["animationState"]
+		);
+	}
 }
