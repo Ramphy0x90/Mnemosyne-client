42c687e57c47d8817b408d51f411c23deddd45bf
ramphy0x90 <ramphy11@hotmail.com>
2023-06-18 14:06:44 +0200

    Item card

---
+++ src/app/components/item-card/item-card.component.ts
@@ -0,0 +1,34 @@
+import {
+	Component,
+	EventEmitter,
+	Input,
+	OnChanges,
+	Output,
+	SimpleChanges,
+} from "@angular/core";
+
+@Component({
+	selector: "app-item-card",
+	templateUrl: "./item-card.component.html",
+	styleUrls: ["./item-card.component.css"],
+})
+export class ItemCardComponent implements OnChanges {
+	@Input() img?: string;
+	@Input() title?: string;
+	@Input() showSelector: boolean = false;
+
+	@Output() selected = new EventEmitter<boolean>();
+
+	isSelected: boolean = false;
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes["showSelector"]) {
+			this.isSelected = false;
+		}
+	}
+
+	onSelect(): void {
+		this.isSelected = !this.isSelected;
+		this.selected.emit(this.isSelected);
+	}
+}
