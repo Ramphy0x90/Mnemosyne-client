069b2cb39917d36af2b8f1a334b5898dce8e2068
ramphy0x90 <ramphy11@hotmail.com>
2023-06-19 21:27:04 +0200

    Test

--- src/app/containers/dev/test/test.component.ts
+++ src/app/containers/dev/test/test.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { AuthTokenData } from "src/app/models/auth/auth-token-data";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+	selector: "app-test",
+	templateUrl: "./test.component.html",
+	styleUrls: ["./test.component.css"],
 })
 export class TestComponent implements OnInit {
+	userData?: AuthTokenData;
 
-  constructor() { }
+	constructor(
+		private authService: AuthService,
+		private jwtHelperService: JwtHelperService
+	) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		let token: string | null = this.authService.getUserToken();
 
+		if (token) {
+			let tokenDecoded = this.jwtHelperService.decodeToken(token);
+
+			this.userData = {
+				username: tokenDecoded.sub,
+				authorities: tokenDecoded.authorities,
+			};
+		}
+	}
 }
